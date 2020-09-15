e8da40e35edaf0d00997ebf8ac4a80b5339e66a0
Adrian <adis112211@gmail.com>
2020-09-15 21:29:13 +0200

    Pierwszy test jednostkowy, mapwoanie url i atrapa widoku

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -1,5 +1,7 @@
-"""superlists URL Configuration
 
+
+
+"""super
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
@@ -10,12 +12,14 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
+    1. Import the include() function: from django.urls import include, pathcd ..
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf.urls import include, url
+from lists import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^$', views.home_page, name='home'),
+    #url(r'^admin/', include(admin.site.urls)),
 ]
