b76b3aa45366c9cc60cddd53f2196de3f44c9935
Adrian <adis112211@gmail.com>
2020-09-16 22:00:25 +0200

    Test funkcjonalny teraz sprawdza mozliwosc dodania elementu listy

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,7 +16,33 @@ class NewVisitorTest(unittest.TestCase):
         # Edyta dowiedziala sie o nowej, wspanialej  aplikacji w postaci listy rzeczy do zrobienia
         # Postawnowila wiec przejsc dna strone glowna tej aplikacji
 
+        #Zwrocila uwage ze tytul strony i naglowek zawiera slowo listy
         self.assertIn('Listy', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Listy', header_text)
+
+        #Od razu zostaje zachecona aby wpisac rzeczy do zrobeina
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            input_box.get_attribute('placeholder'),
+            'Wpisz rzeczy do zrobienia'
+        )
+
+        # W polu tekstowym wpisala "Kupic pawie piora"
+
+        input_box.send_keys('Kupic pawie piora')
+
+        # Po nacisnieciu klawisza Enter strona zostanie uaktualniona i wyswietla
+        #1. Kupic pawie piora
+
+        input_box.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Kupic pawie piora' for row in rows)
+        )
+
+        #Na stronie nadal znajduje sie pole tekstowe zachecajace do podania kolejnego zadania
         self.fail('Zakonczenie testu')
 
 
