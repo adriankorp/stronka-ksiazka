ff24aa3c3650fcb5c8fc0e6a3b02f53d7b5b137e
Adrian <adis112211@gmail.com>
2020-10-16 20:14:39 +0200

    Model dla obiektow Items oraz powiazane z nim migracjie

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -3,7 +3,7 @@ from django.urls import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from .views import home_page
-
+from lists.models import Item
 
 class HomePageTest(TestCase):
 
@@ -18,9 +18,27 @@ class HomePageTest(TestCase):
 
 
     def test_home_page_can_save_POST_request(self):
-        response = self.client.post('/', data={'new_item_text': "Nowy element listy"})
+        response = self.client.post('/', data={'new_item_text':"Nowy element listy"})
 
         self.assertIn('Nowy element listy', response.content.decode(),"Elementu nie ma w liscie")
         self.assertTemplateUsed(response, 'home.html')
 
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'Absolutnie pierwszy element listy'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Drugi element'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'Absolutnie pierwszy element listy')
+        self.assertEqual(second_saved_item.text, 'Drugi element')
 # Create your tests here.p
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 
 def home_page(request):
     return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
+        'new_item_text': request.POST.get('new_item_text', ''),
     })
 # Create your views here.
