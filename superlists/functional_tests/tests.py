1e612f88c2965db39ce671a5998f556c96c47029
Adrian <adis112211@gmail.com>
2020-11-24 20:33:49 +0100

    Test funkcjonalny dla list roznych uzytkownikow

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -42,6 +42,8 @@ class NewVisitorTest(LiveServerTestCase):
         # Po nacisnieciu klawisza Enter strona zostanie uaktualniona i wyswietla
         #1. Kupic pawie piora
         input_box.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         time.sleep(1)
 
         self.check_for_row_in_list_table('1: Kupic pawie piora')
@@ -55,9 +57,27 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('2: Uzyc pawich pior do zrobienia przynety')
         self.check_for_row_in_list_table('1: Kupic pawie piora')
         #Na stronie nadal znajduje sie pole tekstowe zachecajace do podania kolejnego zadania
-        self.fail('Zakonczenie testu')
 
 
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Kupic pawie piora', page_text)
+        self.assertNotIn('zrobienia przynety', page_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Kupic mleko')
+        input_box.send_keys(Keys.ENTER)
+
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url,edith_list_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Kupic pawie piora', page_text)
+        self.assertIn('Kupic mleko', page_text)
 
 
 
