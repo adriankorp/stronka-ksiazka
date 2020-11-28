a6b7d2c55ce7df9ae83a17c658f8f4eb048c8644
Adrian <adis112211@gmail.com>
2020-11-28 21:02:00 +0100

    Changed  files, plus 2 migrations

--- superlists/lists/models.py
+++ superlists/lists/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.db.models import DO_NOTHING
+
+
+class List(models.Model):
+    pass
+
 
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None, on_delete=DO_NOTHING)
 
 # Create your models here.
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -3,7 +3,7 @@ from django.urls import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from .views import home_page, view_list
-from .models import Item
+from .models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -17,16 +17,23 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'Absolutnie pierwszy element listy'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Drugi element'
+        second_item.list = list_
         second_item.save()
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -34,7 +41,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'Absolutnie pierwszy element listy')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Drugi element')
+        self.assertEqual(second_saved_item.list, list_)
 
 
 # Create your tests here.p
@@ -47,8 +56,9 @@ class ListViewTest(TestCase):
 
 
     def test_displys_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
